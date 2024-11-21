ba8178783d03d6dad50c8ff067adc614ed0328b6
Sergey Belkin <belkinserega95@yandex.ru>
2024-11-21 21:01:52 +0300

    1я часть дипломного проекта (реализация сервиса статистики)

---
+++ stats-service/stats-dto/src/main/java/ru/practicum/group/Marker.java
@@ -0,0 +1,15 @@
+package ru.practicum.group;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface Marker {
+
+    interface OnCreate {
+    }
+
+    interface OnUpdate {
+    }
+
+}
+
